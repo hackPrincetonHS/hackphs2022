754dd49e43c51c0d739694ed483646b4506698dd
unknown <knosmos@gmail.com>
2022-08-05 21:06:28 -0400

    UI changes and add CNAME

--- nav.js
+++ nav.js
@@ -1,3 +1,5 @@
 function dropdown() {
     document.getElementById("mobile_menu").classList.toggle("visible");
+    document.getElementById("menu-button").classList.toggle("fa-xmark");
+    document.getElementById("menu-button").classList.toggle("fa-bars");
 }
