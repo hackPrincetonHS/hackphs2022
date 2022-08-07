8f08b0ad27f9c129fdfd5a83e65d0696f1e90cce
unknown <knosmos@gmail.com>
2022-08-07 13:54:56 -0400

    Try to fix scrolling aesthetic elements

--- scroll.js
+++ scroll.js
@@ -1,7 +1,15 @@
+function calcScrollParameters(elem, scrolled) {
+    /* This kinda works, somehow */
+    let rect = elem.getBoundingClientRect();
+    let start = rect.top;
+    let end = rect.top + window.innerHeight;
+    return (end - start) / 100 * scrolled - start;
+}
+
 window.addEventListener("scroll", function() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    document.getElementById("scroll-fish").style.transform = `translateX(${100 - scrolled * 18 + 850}%)`;
-    document.getElementById("scroll-jellyfish").style.transform = `translateX(${scrolled * 18 - 900}%)`;
+    document.getElementById("scroll-fish").style.transform = `translateX(${100 - calcScrollParameters(document.getElementById("scroll-fish"), scrolled)}%)`;
+    document.getElementById("scroll-jellyfish").style.transform = `translateX(${calcScrollParameters(document.getElementById("scroll-jellyfish"), scrolled)}%)`;
 });
